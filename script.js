1095ebac21ef424432f75846ef109aac880e85c8
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-01-28 14:29:08 +0900

    refactoring start

---
+++ script.js
@@ -0,0 +1,53 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+
+// Disable/Enable Button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+
+// Passing Joke to to VoiceRSS API
+function tellMe(joke) {
+    const jokeString = joke.trim().replace(/ /g, '%20');
+    VoiceRSS.speech({
+        key: '39a82d031069455eb4e2f86e8200b017',
+        src: jokeString,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0,
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
+
+
+// Get Jokes from Joke API
+async function getJokes() {
+    let joke = ''
+    const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        console.log(joke)
+        // Text-to-Speech
+        tellMe(joke);
+        // Disable Button
+        toggleButton();
+    } catch (error) {
+        // Catch Errors Here
+        console.log('fetch failed', error);
+    }
+}
+
+
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
